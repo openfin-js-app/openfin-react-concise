b5dd76a1e34a44be4b16e2d05f076fc85aea4a4d
Wentao LI <liwentao90@yahoo.com>
2019-04-18 14:09:56 -0400

    - initially add deps - initially integrate typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import * as React  from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+const App:React.FunctionComponent<{}>=(
+    {}
+)=> {
+  return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -12,17 +13,16 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
           >
             Learn React
           </a>
         </header>
       </div>
-    );
-  }
+  );
 }
 
 export default App;
